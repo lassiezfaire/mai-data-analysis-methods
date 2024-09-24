fe55e2c3a519054a9e718822156eb0d89dfeb434
Lassiez Faire <lassiezfaire6@gmail.com>
2024-09-24 13:40:17 +0300

    initial graph

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import time
 
 from dotenv import load_dotenv
 import requests
+import matplotlib.pyplot as plt
+import networkx as nx
 
 load_dotenv()
 
+
+#
 def get_friends(user_id, access_token):
     url = 'https://api.vk.com/method/friends.get'
     params = {
@@ -24,17 +28,35 @@ def get_friends(user_id, access_token):
     return data['response']
 
 
-user_id = '711398942'
-access_token = os.getenv("TOKEN")
+friends_graph = {}
 
-friends = get_friends(user_id, access_token)
-print(f"Друзья пользователя {user_id}: {friends}")
+my_id = '711398942'
+access_token = os.getenv('TOKEN')
 
+my_friends = get_friends(my_id, access_token)
+friends_graph[my_id] = [str(user_id) for user_id in my_friends.get('items')]
 
-friend_list = friends.get('items')
+friend_list = my_friends.get('items')
 
-for user_id in friend_list:
-    friends = get_friends(user_id, access_token)
-    print(f"Друзья пользователя {user_id}: {friends}")
+for index, user_id in enumerate(friend_list):
+    user_friends = get_friends(user_id, access_token)
+    friends_graph[str(user_id)] = [str(user_id) for user_id in user_friends.get('items')]
     time.sleep(1)
+
+print(friends_graph)
+
+# Создаем направленный граф
+G = nx.DiGraph()
+
+# Добавляем рёбра в граф
+for key, values in friends_graph.items():
+    for value in values:
+        G.add_edge(key, value)
+
+# Рисуем граф
+plt.figure(figsize=(8, 6))
+pos = nx.spring_layout(G)  # Позиции вершин
+nx.draw(G, pos, with_labels=True, node_size=2000, node_color='lightblue', font_size=10, font_weight='bold', arrows=True)
+plt.title("Граф на основе словаря")
+plt.show()
 
